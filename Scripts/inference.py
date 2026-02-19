093ed8e373441b9809a1b3564c66741682228fcc
Hamza <hamza.wasim835@gmail.com>
2026-02-19 12:43:38 +0300

    Adding streamer

--- Scripts/inference.py
+++ Scripts/inference.py
@@ -15,15 +15,16 @@ model.load_adapter("/content/professional_persona_lora")
 
 # Initializing streamer for a more responsive experience
 streamer = TextStreamer(tokenizer, skip_prompt=True)
+stop_token_id = tokenizer.convert_tokens_to_ids("<|eot_id|>")
 # Defining promting functionality
 def ask_professional(question):
     inputs = tokenizer([f"User: {question}\nAssistant:"], return_tensors="pt").to("cuda")
-    outputs = model.generate(**inputs, streamer = streamer, max_new_tokens=128, use_cache = True)
+    outputs = model.generate(**inputs, streamer = streamer, max_new_tokens=128, use_cache = True, eos_token_id = stop_token_id, pad_token_id = tokenizer.eos_token_id)
     return tokenizer.decode(outputs[0], skip_special_tokens=True)
 
 # Loop for prompting user and responding to user
 while (True):
     question = input("How can I help you today(or type quit to exit the chat): ")
-    if (question.lower() == "quit"):
-        break
+    if question.lower() == "quit": break
+
     print(ask_professional(question), flush= True)
