401ed1bd5a4c7a17030fb525cbda7a3da99939a0
Hamza <hamza.wasim835@gmail.com>
2026-02-21 16:13:50 +0300

    LoRA non-activation fix applied

--- Scripts/inference.py
+++ Scripts/inference.py
@@ -18,9 +18,18 @@ streamer = TextStreamer(tokenizer, skip_prompt=True)
 
 # Defining promting functionality
 def ask_professional(question):
-    inputs = tokenizer([f"User: {question}\nAssistant:"], return_tensors="pt").to("cuda")
+    sysPrompt = f"""<|begin_of_text|><|start_header_id|>system<|end_header_id|>
+                You are a firm, empathetic professional. You acknowledge customer frustration without apologizing and then 
+                pivot to a logical solution. No matter what happens, No matter how angry, frustrated, or insulting the 
+                customer behaves, you stay polite and stick to company policy.<|eot_id|><|start_header_id|>user<|end_header_id|>
+                {question}<|eot_id|><|start_header_id|>assistant<|end_header_id|>"""
+    
+    inputs = tokenizer([sysPrompt], return_tensors="pt").to("cuda")
     outputs = model.generate(**inputs, streamer = streamer, max_new_tokens=128, use_cache = True)
-    return tokenizer.decode(outputs[0], skip_special_tokens=True)
+    generated_tokens = outputs[0][inputs["input_ids"].shape[-1]:]
+    response = tokenizer.decode(generated_tokens, skip_special_tokens=True)
+    return response.strip()
+    
 
 # Loop for prompting user and responding to user
 while (True):
